6e67142530f87daccd5d9671dde14e632e307306
somoscddtecnologia <somoscddtecnologia@gmail.com>
2025-07-10 22:00:22 -0300

    organizando app

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
-const express = require("express")
-const mongoose = require("mongoose")
-const contatoRoutes = require("./routes/contatoRoutes")
-const authRoutes = require("./routes/authRoutes")
 
+const express = require('express')
+const mongoose = require('mongoose')
 const swaggerUi = require('swagger-ui-express')
+
 const swaggerSpec = require('./docs/swagger')
 
+const contatoRoutes = require("./routes/contatoRoutes")
+const authRoutes = require("./routes/authRoutes")
 
 
 const app = express()
@@ -17,11 +18,10 @@ app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 mongoose.connect(process.env.MONGODB_URL)
     .then(() => console.log("MongoDB Conectado"))
-    .catch((err) => console.log("MongoDB mão conectado", err))
+    .catch((err) => console.log("MongoDB não conectado:", err.message))
 
 app.use('/', contatoRoutes)
 app.use('/auth', authRoutes)
-
 
 
 const PORT = process.env.PORT
